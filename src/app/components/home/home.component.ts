add6ef1d249cfb4c74d7cdf1d09a357ae1c20cba
emrepektemek <okyanus5401@hotmail.com>
2025-01-29 10:40:12 +0300

    Home Component header oluşturuldu, çıkış işlemi yapıldı

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  constructor(
+    private authService: AuthService,
+    private toastrService: ToastrService,
+    private router: Router
+  ) {}
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+    this.toastrService.info('Çıkış Yapıldı');
+  }
+  goToHome() {
+    this.router.navigate(['/home']);
+  }
 }

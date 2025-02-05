c06f6d439f01a22f55ca8085065e1384fdce64c4
emrepektemek <okyanus5401@hotmail.com>
2025-02-05 09:53:29 +0300

    Order component search bar oluşturuldu

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -3,16 +3,19 @@ import { OrderReportModel } from './../../models/orderReportModel';
 import { CommonModule } from '@angular/common';
 import { OrderReportState } from './../../store/order-report.state';
 
+import { FormsModule } from '@angular/forms';
+
 import { take } from 'rxjs';
 
 @Component({
   selector: 'app-order',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './order.component.html',
   styleUrl: './order.component.css',
 })
 export class OrderComponent implements OnInit {
   orderReports: OrderReportModel[] = [];
+  searchText: string = '';
   dataLoaded: boolean = false;
 
   constructor(private orderReportState: OrderReportState) {}
@@ -22,5 +25,21 @@ export class OrderComponent implements OnInit {
       this.orderReports = reports;
       this.dataLoaded = true;
     });
+  }
+
+  get filteredOrders(): OrderReportModel[] {
+    if (!this.searchText) {
+      return this.orderReports;
+    }
+
+    const lowerCaseSearch = this.searchText.toLowerCase();
+
+    return this.orderReports.filter(
+      (order) =>
+        order.customerName.toLowerCase().includes(lowerCaseSearch) ||
+        order.customerEmail.toLowerCase().includes(lowerCaseSearch) ||
+        order.customerAddress.toLowerCase().includes(lowerCaseSearch) ||
+        order.customerPhoneNumber.includes(lowerCaseSearch)
+    );
   }
 }
